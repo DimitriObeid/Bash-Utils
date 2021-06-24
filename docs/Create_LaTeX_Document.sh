7de96fa2763fa445b0045cb7aa6a162484138519
DimitriObeid <obeiddimitri@gmail.com>
2021-06-24 15:53:57 +0200

    Updated the Create_LaTeX_Document file

--- docs/Create_LaTeX_Document.sh
+++ docs/Create_LaTeX_Document.sh
@@ -1,20 +1,92 @@
 #!/usr/bin/env bash
 
+# ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; #
+
+########################################### SOURCING PROJECT'S DEPENDENCIES ###########################################
+
 source "$HOME/.bash_profile" || { echo >&2; echo "Unable to source the \"$HOME/.bash_profile\" file" >&2; echo >&2; exit 1 }
 source "$__BASH_UTILS/Initializer.sh" || { echo >&2; echo "Unable to source the \"$__BASH_UTILS/Initializer.sh\" file" >&2; echo >&2; exit 1 }
 
 
+
+# ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; #
+
+######################################################### CODE ########################################################
+
+## FUNCTIONS DEFINITION
+
+
+# ----------------------------------------------
+
+## VARIABLES DEFINITION
+
+# Supported languages array.
 __supported_languages=('en', 'fr')
 
-EchoNewstep "Please type the wanted language's code in the following parenthesis --> English (en), French (fr)"
-read -rp "In which language do you want to write your LaTeX document ?" __lang
-EchoRead "$__lang"
 
-lineno="$LINENO"; if [[ "$__lang" =~ "$__supported_languages[*]" ]]; then
+# -----------------------------------------------
+
+## STATUS VARIABLES MODIFICATION
+
+$__STAT_ERROR="fatal";CheckSTAT_ERROR "$(basename "${BASH_SOURCE[0]}")" "$LINENO"
+
+# -----------------------------------------------
+
+## CODE
+
+EchoNewstep "Please type the wanted language's code in the following parenthesis --> English (en), French (fr)"
+read -rp "In which language do you want to write your LaTeX document ?" __read_lang
+EchoRead "$__read_lang"
+
+lineno_if_valid_lang="$LINENO"; if [[ "$__read_lang" =~ "$__supported_languages[*]" ]]; then
+
+	#***** Conditions variables definition.
+	__path1="$__lang/Bash/"
+	__path2="$__read_lang/Bash/functions/basis"
+	__path3="$__read_lang/Bash/functions/main"
+	__path4="$__read_lang/Bash/variable"
+
+	#***** Asking for the new document's path.
+	EchoNewstep "What kind of document do you want to write ?"
+	EchoMsg "1 - Master documentation			Targeted folder --> $__path1"
+	EchoMsg "2 - Basic functions documentation	Targeted folder --> $__path2"
+	EchoMsg "3 - Main functions documentation	Targeted folder --> $__path3"
+	EchoMsg "4 - Variables documentation		Targeted folder --> $__path4"
+	Newline
+
+	read -rp "Please type the number corresponding to the wanted document category." __read_folder_code
+	EchoRead "$__read_folder_code"
 	
+	#***** Verifying if the entered code is valid.
+	lineno_case_read_folder_is_valid="$LINENO"; case "$__read_folder_code" in
+		1)
+			__folder_path="$__read_lang/$__path1"
+			;;
+		2)
+			__folder_path="$__read_lang/$__path2"
+			;;
+		3)
+			__folder_path="$__read_lang/$__path3"
+			;;
+		4)
+			__folder_path="$__read_lang/$__path4"
+			;;
+		*)
+			HandleErrors "1" "The \$__read_folder_code entered value is invalid" "Please type an integer value ranging from 1 to 4" "$__read_folder_code" "$(basename "${BASH_SOURCE[0]}")" "${FUNCNAME[0]}" "$lineno_case_read_folder_is_valid"
+			;;
+	esac
 	
+	read -rp "How do you want to name your document ?" __read_doc_name
+	EchoRead "$__read_doc_name"
 	
-	case
+	if [ -z "$__read_doc_name" ]; then
+		HandleErrors "1" "The \$__read_doc_name variable is empty" "Please type a valid name according to your filesystem accepted values" "$__read_doc_name" "$(basename "${BASH_SOURCE[0]}")" "${FUNCNAME[0]}" "$(( LINENO-3 ))"
+	fi
+
+
+
+	Makedir "" ""
+
 else
-	HandleErrors "1" "The \$__lang variable's value is incorrect" "" "$__lang" "${BASH_SOURCE[0]}" "${FUNCNAME[0]}" "$lineno"
+	HandleErrors "1" "The \$__read_lang variable's value is incorrect" "" "$__read_lang" "$(basename "${BASH_SOURCE[0]}")" "${FUNCNAME[0]}" "$lineno_if_valid_lang"
 fi
