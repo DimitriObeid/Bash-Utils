71c9fbdb0b58a9a8d9cffb2413b445353c023c12
DimitriObeid <obeiddimitri@gmail.com>
2021-06-28 17:28:11 +0200

    Moved variables files to config/, created the __STAT_FIFO_REDIRECTION status variable, moved documentation files and modified other files when I was in the train

--- projects/Linux-reinstall/Linux-reinstall.sh
+++ projects/Linux-reinstall/Linux-reinstall.sh
@@ -26,7 +26,7 @@ if ! source "$HOME/.bash_profile"; then
 fi
 
 # shellcheck disable=SC1091
-if ! source "$__BASH_UTILS/Initializer.sh"; then
+if ! source "$__BASH_UTILS_INITIALIZER"; then
     echo >&2; echo "In $(basename "$0"), line $(( LINENO-1 )) --> Error : unable to source the initialization file." >&2; echo >&2; exit 1
 fi
 
