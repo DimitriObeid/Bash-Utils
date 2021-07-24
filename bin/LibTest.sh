36c0d983a1ee2f1fdb066bda92a801e5cdca1300
DimitriObeid <obeiddimitri@gmail.com>
2021-07-24 20:40:29 +0200

    Added interpretation of backslash escapes for every "echo" command

--- bin/LibTest.sh
+++ bin/LibTest.sh
@@ -2,12 +2,12 @@
 
 # shellcheck disable=SC1091
 if ! source "$HOME/.bash_profile"; then
-    echo >&2; echo "In $(basename "$0"), line $(( LINENO-1 )) --> Error : unable to source the ~/.bash_profile file." >&2; echo >&2; exit 1
+    echo >&2; echo -e "In $(basename "$0"), line $(( LINENO-1 )) --> Error : unable to source the ~/.bash_profile file." >&2; echo >&2; exit 1
 fi
 
 # shellcheck disable=SC1091
 if ! source "$__BASH_UTILS_LIB_FILE_INITIALIZER"; then
-    echo >&2; echo "In $(basename "$0"), line $(( LINENO-1 )) --> Error : unable to source the initialization file." >&2; echo >&2; exit 1
+    echo >&2; echo -e "In $(basename "$0"), line $(( LINENO-1 )) --> Error : unable to source the initialization file." >&2; echo >&2; exit 1
 fi
 
 
