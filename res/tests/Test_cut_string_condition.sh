fb22e2ff2e50d6af1fbbbda43f2875011557188c
DimitriObeid <obeiddimitri@gmail.com>
2021-07-26 18:32:46 +0200

    Saving after creating a failsafe for the "Decho<...>()" functions

---
+++ res/tests/Test_cut_string_condition.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$HOME/Projets/Bash-utils/lib/functions/main/Text.lib" || { echo "Fail source"; exit 1; }
+
+function test_condition()
+{
+	if [ "$1" = "$(CutDashFromOption "$1")" ]; then
+		echo "$1"
+	else
+		echo "Fail : $1"
+	fi
+}
+
+test_condition "-n"
+
