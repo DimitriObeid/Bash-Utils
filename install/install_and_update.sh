04b762e89cab2c1e04aa94596927be09fc541f05
Dimitri Obeid <obeiddimitri@gmail.com>
2021-08-10 00:52:46 +0200

    Improved again the installation and update scripts

--- install/install_and_update.sh
+++ install/install_and_update.sh
@@ -12,6 +12,18 @@ fi
 # Feel free to add any user's home directory path into the "users.list" file.
 mapfile -t __TARGET_HOME_DIRECTORIES < "users.list"
 
+## FUNCTIONS
+
+# Printing a line according to the terminal's columns number.
+function PrintLine()
+{
+    __cols="$(tput cols)"
+
+    for _ in $(eval echo -e "{1..$__cols}"); do
+            printf '-'
+    done; printf "\n\n"
+}
+
 # ------------------------------------------------
 
 ## DIRECTORIES AND FILES NAMES
@@ -24,6 +36,7 @@ __INSTALL_DIRECTORY_SOURCE="$(pwd -P "$(basename "${BASH_SOURCE[0]}")")"
 
 __INSTALL_MODULE_DIRECTORY=".Bash-utils"
 
+__BU_root_path_file_name="Bash-utils-root-val.path"
 
 # ------------------------------------------------
 
@@ -35,18 +48,26 @@ __INSTALL_DIRECTORY_DESTINATION_PATH="$user/$__INSTALL_MODULE_DIRECTORY"
 
 __module_init_file_home_dir="$user/$__module_init_file"
 
+__BU_root_path_file_parent="$user/$__INSTALL_MODULE_DIRECTORY"
+
+__BU_root_path_file_path="$__BU_root_path_file_parent/$__BU_root_path_file_name"
+
 # ------------------------------------------------
 
 ## CODE
 
-printf "Copying the Bash Utils root directory path into the %s file\n" "$__BU_root_path_file"
+printf "Copying the Bash Utils root directory path into the %s file\n" "$__BU_root_path_file" 0"0"
 printf "%s" "$(cd "$(dirname "$(basename "${BASH_SOURCE[0]}")")"; cd ..; pwd -P)" 2>&1 | tee "$__BU_root_path_file" || { printf "UNABLE TO WRITE THE BASH UTILS ROOT DIRECTORY PATH INTO THE %s FILE" "$__BU_root_path_file"; exit 1; }
 printf "\n\nSuccessfully copied the Bash Utils root directory path into the %s file\n\n" "$__BU_root_path_file"
+
+PrintLine
 
 for user in "${__TARGET_HOME_DIRECTORIES[@]}"; do
 	if [ ! -d "$user" ]; then
 		printf "ERROR --> %s IS NOT AN EXISTING USER !\n\n" "$user"; exit 1
 	else
+		printf "PROCESSED USER : %s\n\n" "${user##*/}"
+
 		printf "Copying the modules initialization file into the %s directory\n" "$user"
 
 		if [ -f "$__module_init_file_home_dir" ]; then
@@ -64,11 +85,19 @@ for user in "${__TARGET_HOME_DIRECTORIES[@]}"; do
             		# Check this link for more informations about this command --> https://github.com/koalaman/shellcheck/wiki/SC2115
             		rm -rfv "${$__INSTALL_DIRECTORY_DESTINATION_PATH/:?}/"* || { printf "UNABLE TO OVERWRITE THE HIDDEN %s MODULES MANAGER'S DIRECTORY !\n\n" "$__INSTALL_DIRECTORY_DESTINATION_PATH"; exit 1; }
 			cp -rv "$__INSTALL_MODULE_DIRECTORY_FULL_PATH" "$user" || { printf "UNABLE TO COPY THE %s  DIRECTORY INTO THE $user DIRECTORY !\n\n" "$__INSTALL_MODULE_DIRECTORY_FULL_PATH"; exit 1; }
+
+			if [ -f "$__BU_root_path_file_path" ]; then
+				cat "$__BU_root_path_file_name" 2>&1 "$__BU_root_path_file_path" || { printf "UNABLE TO OVERWRITE THE %s FILE WITH THE %s FILE'S CONTENT !\n\n" "$__BU_root_path_file_path" "$__BU_root_path_file_name"; exit 1; }
+			else
+				cp -v "$__BU_root_path_file_name" "$__BU_root_path_file_path" || { printf "UNABLE TO COPY THE %s FILE IN THE %s DIRECTORY\n\n" "$__BU_root_path_file_name" "$__BU_root_path_file_parent"; exit 1; }
+			fi
 		else
 			cp -rv "$__INSTALL_MODULE_DIRECTORY_FULL_PATH" "$user" || { printf "UNABLE TO COPY THE %s  DIRECTORY INTO THE $user DIRECTORY !\n\n" "$__INSTALL_MODULE_DIRECTORY_FULL_PATH"; exit 1; }
 		fi
 
-		printf "\n\nTHE INSTALLATION / UPDATE OF THE MODULES MANAGER IS DONE FOR THE ${user##*/} USER !\n\n\n"
+		printf "\n\nTHE INSTALLATION / UPDATE OF THE MODULES MANAGER IS DONE FOR THE ${user##*/} USER !\n\n"
+
+		PrintLine
 	fi
 done
 
--- install/update_path.sh
+++ install/update_path.sh
@@ -1,17 +1,43 @@
 #!/usr/bin/env bash
 
+if [ "$EUID" -ne 0 ]; then
+        printf "THIS INSTALL PATH UPDATER SCRIPT MUST BE EXECUTED WITH SUPER-USER PRIVILEGES !\n\n"; exit 1
+fi
+
 # Feel free to add any user's home directory path into the "users.list" file.
 mapfile -t __TARGET_HOME_DIRECTORIES < "users.list"
 
-__BU_root_path_file="Bash-utils-root-val.path"
+__BU_root_path_file_name="Bash-utils-root-val.path"
 
-printf "Copying the Bash Utils root directory path into the %s file\n" "$__BU_root_path_file"
-printf "%s" "$(cd "$(dirname "$(basename "${BASH_SOURCE[0]}")")"; cd ..; pwd -P)" 2>&1 | tee "$__BU_root_path_file" || { printf "UNABLE TO WRITE THE BASH UTILS ROOT DIRECTORY PATH INTO THE %s FILE" "$__BU_r>
-printf "\n\nSuccessfully copied the Bash Utils root directory path into the %s file\n\n" "$__BU_root_path_file"
+__BU_module_directory_name=".Bash-utils"
+
+__BU_root_dir="$(cd "$(dirname "$(basename "${BASH_SOURCE[0]}")")"; cd ..; pwd -P)"
+
+__BU_root_path_file_old_parent="$(pwd -P)/$__BU_module_directory_name"
+__BU_root_path_file_old_path="$__BU_root_path_file_old_parent/$__BU_root_path_file_name"
+
+printf "Copying the Bash Utils root directory path into the %s file\n\n" "$__BU_root_path_file_old_path"
+printf "Library path : %s\n\n" "$__BU_root_dir"
 
 for user in "${__TARGET_HOME_DIRECTORIES[@]}"; do
         if [ ! -d "$user" ]; then
                 printf "ERROR --> %s IS NOT AN EXISTING USER !\n\n" "$user"; exit 1
         else
-		
-fi
+
+        __BU_root_path_file_new_parent="$user/$__BU_module_directory_name"
+        __BU_root_path_file_new_path="$__BU_root_path_file_new_parent/$__BU_module_directory_name"
+
+	__cols="$(tput cols)"
+
+	for _ in $(eval echo -e "{1..$__cols}"); do
+		printf '-'
+	done; printf "\n\n"
+
+	printf "PROCESSED USER'S PATH FILE : %s\n\n" "${user##*/}"
+
+        printf "%s" "$__BU_root_dir" 2>&1 | tee "$__BU_root_path_file_new_path" || { printf "\n\nUNABLE TO WRITE THE BASH UTILS ROOT DIRECTORY PATH INTO THE %s FILE\n\n" "$__BU_root_path_file_new_path"; exit 1; }
+        printf "\n\nSuccessfully copied the Bash Utils root directory path into the %s file\n\n" "$__BU_root_path_file_new_path"
+	fi
+done
+
+printf "\nDONE!\n\n"
