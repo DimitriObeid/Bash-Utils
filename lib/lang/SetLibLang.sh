b65a6a8a6e1de1ba6d2fbb8104770209b0cbfd9f
Dimob <obeiddimitri@gmail.com>
2021-12-14 14:53:03 +0100

    Updating the French documentation and improved the SetLibLang.sh script

--- lib/lang/SetLibLang.sh
+++ lib/lang/SetLibLang.sh
@@ -10,7 +10,7 @@ function SetLibLang()
             # Setting the appropriate CSV file's translation's column number (from the ".Bash-utils/config/module/main/LangCSVCode.conf" file).
             __BU_MAIN_LANG_CSV_CODE="$__BU_MAIN_LANG_CSV_CODE_EN"
 
-            ParseCSVLibLang "$__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv" "$LANG" \
+            ParseCSVLibLang "$__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv" "$__BU_MAIN_LANG" \
                 "Translation file found : $__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv" \
                 "ERROR : CANNOT FIND THE $(DechoHighlight "$__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv") FILE" \
                 "Please make sure that the file path passed as $(DechoHighlight "${FUNCNAME[0]}") function's first argument is valid"
@@ -21,7 +21,7 @@ function SetLibLang()
             # Définition du numéro de colonne approprié pour la traduction du fichier CSV (depuis le fichier ".Bash-utils/config/module/main/LangCSVCode.conf").
             __BU_MAIN_LANG_CSV_CODE="$__BU_MAIN_LANG_CSV_CODE_FR"
 
-            ParseCSVLibLang "$__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv" "$LANG" \
+            ParseCSVLibLang "$__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv" "$__BU_MAIN_LANG" \
 				"Fichier de traduction trouvé : $__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv" \
                 "ERREUR : IMPOSSIBLE DE TROUVER LE FICHIER $(DechoHighlight "$__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv")" \
                 "Assurez-vous que le chemin du fichier passé en premier argument de la fonction $(DechoHighlight "${FUNCNAME[0]}") soit valide"
@@ -45,10 +45,10 @@ function SetLibLang()
             EchoMsg "YOUR CURRENT LANGUAGE IS NOT YET SUPPORTED !!" "1"
             EchoMsg "The $__BU_MAIN_PROJECT_NAME library language will be set in English" "1"
 
-            # Changing the "$LANG" environment variable's value for the mandatory arguments call.
-            LANG="en_US.UTF-8"
+            # Changing the "$__BU_MAIN_LANG" variable's value for the mandatory arguments call.
+            __BU_MAIN_LANG="en_US.UTF-8"
 
-            ParseCSVLibLang "$__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv" "$LANG" \
+            ParseCSVLibLang "$__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv" "$__BU_MAIN_LANG" \
                 "Translation file found : $__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv" \
                 "ERROR : CANNOT FIND THE $(DechoHighlight "$__BU_MAIN_MODULE_LIB_LANG_DIR_PATH/lang.csv") FILE" \
                 "Please make sure that the file path passed as $(DechoHighlight "${FUNCNAME[0]}") function's first argument is valid"
@@ -67,9 +67,11 @@ function GetLibLang()
     # In case an user would like to use another language instead of the current used language in the system,
     # he/she can create a file named "lang.conf" in the "Bash-Utils/config" modules files' folder and write his/her language's ISO 639-1 code.
     if [ -f "$__BU_MAIN_MODULE_CONF_LANG_ISO_CODE_PATH" ] && [ -s "$__BU_MAIN_MODULE_CONF_LANG_ISO_CODE_PATH" ]; then
-        LANG="$(cat "$__BU_MAIN_MODULE_CONF_LANG_ISO_CODE_PATH")"
+        __BU_MAIN_LANG="$(cat "$__BU_MAIN_MODULE_CONF_LANG_ISO_CODE_PATH")"
         SetLibLang
     else
+    	# If the file doesn't exists or is empty, then the system language is chosen by the program.
+    	__BU_MAIN_LANG="$LANG"
         SetLibLang
     fi
 }
