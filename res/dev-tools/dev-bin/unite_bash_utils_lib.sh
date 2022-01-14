d53f8f018288f8540dd400efaecfad0a295aac17
Dimob <obeiddimitri@gmail.com>
2022-01-14 23:57:13 +0100

    Improved the Bash utils uniter script's statistics display

--- res/dev-tools/dev-bin/unite_bash_utils_lib.sh
+++ res/dev-tools/dev-bin/unite_bash_utils_lib.sh
@@ -18,7 +18,7 @@ function PrintLine()
 
 # function BU::Newline { local iterations="$1"; for ((i=0; i<iterations; i++)); do echo -e "" | tee -a "$__BU_MAIN_FULL_FILE_PATH"; done; }
 function CatBU { cat "$1" | tee -a "$__BU_MAIN_FULL_FILE_PATH" || { echo "UNABLE TO DISPLAY THE $1 FILE'S CONTENT ! Please check its path and if it exists."; exit 1; }; }
-function BU::EchoBU { echo -e "# $1" | tee -a "$__BU_MAIN_FULL_FILE_PATH" || { echo "UNABLE TO WRITE THE $1 FILE'S CONTENT ! Please check its path and if it exists."; exit 1; }; }
+function EchoBU { echo -e "# $1" | tee -a "$__BU_MAIN_FULL_FILE_PATH" || { echo "UNABLE TO WRITE THE $1 FILE'S CONTENT ! Please check its path and if it exists."; exit 1; }; }
 
 if [ ! -f "$__BU_MAIN_FULL_FILE_PATH" ]; then
 	touch "$__BU_MAIN_FULL_FILE_PATH"
@@ -30,33 +30,33 @@ fi
 
 
 # Processing the modules initializer file.
-BU::EchoBU "$HOME/Bash-utils-init.sh"; BU::Newline '1'; CatBU "$i"
+EchoBU "$HOME/Bash-utils-init.sh"; BU::Newline '1'; CatBU "$i"
 
 # Processing the modules configuration files.
 for i in "$HOME/.Bash-utils/config/modules/main/"*.conf; do
-	BU::Newline '2'; :' BU::EchoBU "${i^^}"; BU::Newline "1"'; CatBU "$i"
+	BU::Newline '2'; :' EchoBU "${i^^}"; BU::Newline "1"'; CatBU "$i"
 done
 
 # Processing the modules files.
 for i in "$HOME/.Bash-utils/modules/main/"*; do
-	BU::Newline '2'; :' BU::EchoBU "${i^^}"; BU::Newline "1"'; CatBU "$i"
+	BU::Newline '2'; :' EchoBU "${i^^}"; BU::Newline "1"'; CatBU "$i"
 done
 
 # Processing the function files.
 for i in "$__BU_ROOT_PATH/lib/functions/main/"*.lib; do
-	BU::Newline '2'; :' BU::EchoBU "${i^^}"; BU::Newline "1"'; CatBU "$i"
+	BU::Newline '2'; :' EchoBU "${i^^}"; BU::Newline "1"'; CatBU "$i"
 done
 
 # Processing the remaining files.
-BU::Newline '2'; :'BU::EchoBU "$__BU_ROOT_PATH/lib/lang/SetLibLang.sh"; BU::Newline "1"'; CatBU "$__BU_ROOT_PATH/lib/lang/SetLibLang.sh"
+BU::Newline '2'; :'EchoBU "$__BU_ROOT_PATH/lib/lang/SetLibLang.sh"; BU::Newline "1"'; CatBU "$__BU_ROOT_PATH/lib/lang/SetLibLang.sh"
 
 
 PrintLine
 
-printf "Library statistics :\n\n"
+echo "Library statistics :"; echo
 
-printf "Size in bytes		    : %s\t\t\n" "$(wc -c "$__BU_MAIN_FULL_FILE_PATH")"
-printf "Number of characters 	: %s\t\t\n" "$(wc -m "$__BU_MAIN_FULL_FILE_PATH")"
-printf "Number of lines		    : %s\t\t\n" "$(wc -l "$__BU_MAIN_FULL_FILE_PATH")"
-printf "Maximum display width 	: %s\t\t\n" "$(wc -L "$__BU_MAIN_FULL_FILE_PATH")"
-printf "Number of words 	    : %s\t\t\n" "$(wc -w "$__BU_MAIN_FULL_FILE_PATH")"
+echo "Size in bytes           : $(wc -c < "$__BU_MAIN_FULL_FILE_PATH") bytes"
+echo "Number of characters    : $(wc -m < "$__BU_MAIN_FULL_FILE_PATH") characters"
+echo "Number of lines         : $(wc -l < "$__BU_MAIN_FULL_FILE_PATH") lines"
+echo "Maximum display width   : $(wc -L < "$__BU_MAIN_FULL_FILE_PATH") columns"
+echo "Number of words         : $(wc -w < "$__BU_MAIN_FULL_FILE_PATH") words"
