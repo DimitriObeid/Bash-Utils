40ba031da07133d2e92d37937d66703b87155e6c
Dimob <obeiddimitri@gmail.com>
2022-02-12 17:05:21 +0100

    Reworking on the LaTeX files generator script

--- res/dev-tools/dev-bin/latex-create-document.sh
+++ res/dev-tools/dev-bin/latex-create-document.sh
@@ -26,12 +26,21 @@ fi
 
 # Please define any needed arguments here.
 
+# Targeted language
+__ARG_LANG=$1
+
+# Type of document (master documentation, configurations, module initializer resources or library functions).
+__ARG_TYPE=$2
+
+# Name of the file to create.
+__ARG_FILENAME=$3
+
 # -----------------------------------------------
 
 ## VARIABLES DEFINITION
 
 # Script's resources directory.
-__RES_DIR="$__BU_MAIN_MODULE_DOCS_DIR_PATH/.$__BU_MAIN_PROJECT_NAME"
+__RES_DIR="$__BU_MAIN_MODULE_DEVTOOLS_SRC/$__BU_MAIN_PROJECT_NAME"
 
 # Supported languages array.
 __supported_languages=('en (English)' 'fr (French | Français)')
@@ -47,7 +56,7 @@ ChangeSTAT_ERROR 'fatal' "$(basename "${BASH_SOURCE[0]}")" "$LINENO"
 ## FUNCTIONS DEFINITION
 
 # Listing the supported languages.
-function LatexCreateDoc:BU::ListLanguages()
+function LatexCreateDoc_ListLanguages()
 {
     BU::EchoNewstep "Currently supported languages --> English (en), French (fr)"
 
@@ -61,7 +70,7 @@ function LatexCreateDoc:BU::ListLanguages()
 ## CODE
 
 BU::EchoNewstep "In which language do you want to write your LaTeX document ?"
-LatexCreateDoc:BU::ListLanguages
+LatexCreateDoc_ListLanguages
 BU::Newline
 
 read -rp "Please type the wanted language's code from one of the above parenthesis : " __read_lang
@@ -70,56 +79,83 @@ BU::Newline
 
 if [[ "$__read_lang" =~ ${__supported_languages[*]} ]]; then
 	# shellcheck disable=SC2016
-	BU::Main::Errors::HandleErrors "1" "THE $(BU::Main::Echo::ToLowercase "$(BU::DechoHighlightError '$__read_doc_name')'s") VARIABLE'S VALUE IS INCORRECT" \
-        "$(LatexCreateDoc:BU::ListLanguages)" "$__read_lang" "$(basename "${BASH_SOURCE[0]}")" "${FUNCNAME[0]}" "$(( LINENO-1 ))"
+	BU::Main::Errors::HandleErrors "1" "THE $(BU::DechoHighlightError '$__read_doc_name')'S VARIABLE'S VALUE $(DechoHighlight "") IS INCORRECT" \
+        "$(LatexCreateDoc_ListLanguages)" "$__read_lang" "$(basename "${BASH_SOURCE[0]}")" "${FUNCNAME[0]}" "$(( LINENO-1 ))"
+
+elif [[ "$__ARG_LANG" =~ ${__supported_languages[*]} ]]; then
+# shellcheck disable=SC2016
+	BU::Main::Errors::HandleErrors "1" "THE FIRST ARGUMENT'S VALUE (LANGUAGE) IS INCORRECT" \
+        "$(LatexCreateDoc_ListLanguages)" "$__read_lang" "$(basename "${BASH_SOURCE[0]}")" "${FUNCNAME[0]}" "$(( LINENO-1 ))"
+
 else
 
 	#***** Conditions variables definition.
-	__path_Bash="$__read_lang/Bash/"
-	__path_config="$__read_lang/config"
-	__path_basis_functions="$__read_lang/Bash/functions/basis"
-	__path_main_functions="$__read_lang/Bash/functions/main"
-	__path_variables="$__read_lang/Bash/variable"
+	__target_path_master="$__BU_MAIN_MODULE_DOCS_DIR_PATH/$__read_lang"
+	__target_path_module="$__target_path_master/modules"
 
-	#***** Asking for the new document's path.
-	BU::EchoNewstep "What kind of document do you want to write ?"
-	BU::EchoMsg "1 - Master documentation			$(BU::DechoGreen "Targeted folder") --> $(BU::Decho "$__path_Bash")"
-	BU::EchoMsg "2 - Configuration documentation	$(BU::DechoGreen "Targeted folder") --> $(BU::Decho "$__path_config")"
-	BU::EchoMsg "3 - Basic functions documentation	$(BU::DechoGreen "Targeted folder") --> $(BU::Decho "$__path_basis_functions")"
-	BU::EchoMsg "4 - Main functions documentation	$(BU::DechoGreen "Targeted folder") --> $(BU::Decho "$__path_main_functions")"
-	BU::EchoMsg "5 - Variables documentation		$(BU::DechoGreen "Targeted folder") --> $(BU::Decho "$__path_variables")"
-	BU::Newline;
+	__target_path_initscript="$__target_path_module/01) ScriptInit"
+	__target_path_config="$__target_path_module/02) Config"
+	__target_path_init="$__target_path_module/03) Init"
+	__target_path_main_functions="$__target_path_module/04) Functions"
 
-	read -rp "Please type the number corresponding to the wanted document category : " __read_folder_code
-	BU::EchoRead "$__read_folder_code"
-	BU::Newline
+	# If no type of document is passed as second argument
+    if [ -z "$__ARG_TYPE" ]; then
+        #***** Asking for the new document's path.
+        BU::EchoNewstep "What kind of document do you want to write ?"
+        BU::EchoMsg "1 - Master documentation                       $(BU::DechoGreen "Targeted folder") --> $(BU::Decho "$__target_path_master")"
+        BU::EchoMsg "2 - Module general documentation               $(BU::DechoGreen "Targeted folder") --> $(BU::Decho "$__target_path_module")"
+        BU::EchoMsg "3 - Module initializer script's documentation  $(BU::DechoGreen "Targeted folder") --> $(BU::Decho "$__target_path_initscript")"
+        BU::EchoMsg "4 - Module configuration documentation         $(BU::DechoGreen "Targeted folder") --> $(BU::Decho "$__target_path_config")"
+        BU::EchoMsg "5 - Module initializer documentation           $(BU::DechoGreen "Targeted folder") --> $(BU::Decho "$__target_path_init")"
+        BU::EchoMsg "6 - Main functions documentation               $(BU::DechoGreen "Targeted folder") --> $(BU::Decho "$__target_path_main_functions")"
+        BU::Newline;
 
-	#***** Verifying if the entered code is valid.
+    	read -rp "Please type the number corresponding to the wanted document category : " __read_folder_code
+        BU::EchoRead "$__read_folder_code"
+        BU::Newline
+
+    elif [ -n "$__ARG_TYPE" ]; then
+        __read_folder_code="$__ARG_TYPE"
+    fi
+
+	#***** Verifying if the given code is valid.
 	lineno_case_read_folder_is_valid="$LINENO"; case "$__read_folder_code" in
 		1)
-			BU::Main::Directories::Make "$__BU_MAIN_DOCS" "$__path_Bash" && __folder_path="$__BU_MAIN_DOCS/$__path_Bash"
+			BU::Main::Directories::Make "$__BU_MAIN_DOCS" "$__target_path_master" && __folder_path="$__BU_MAIN_DOCS/$__target_path_master"
 			;;
 		2)
-			BU::Main::Directories::Make "$__BU_MAIN_DOCS" "$__path_config" && __folder_path="$__BU_MAIN_DOCS/$__path_config"
+			BU::Main::Directories::Make "$__BU_MAIN_DOCS" "$__target_path_master" && __folder_path="$__BU_MAIN_DOCS/$__target_path_module"
 			;;
 		3)
-			BU::Main::Directories::Make "$__BU_MAIN_DOCS" "$__path_basis_functions" && __folder_path="$__BU_MAIN_DOCS/$__path_basis_functions"
+			BU::Main::Directories::Make "$__BU_MAIN_DOCS" "$__target_path_initscript" && __folder_path="$__BU_MAIN_DOCS/$__target_path_initscript"
 			;;
 		4)
-			BU::Main::Directories::Make "$__BU_MAIN_DOCS" "$__path_main_functions" && __folder_path="$__BU_MAIN_DOCS/$__path_main_functions"
+			BU::Main::Directories::Make "$__BU_MAIN_DOCS" "$__target_path_config" && __folder_path="$__BU_MAIN_DOCS/$__target_path_config"
 			;;
 		5)
-			BU::Main::Directories::Make "$__BU_MAIN_DOCS" "$__path_variables" && __folder_path="$__BU_MAIN_DOCS/$__path_variables"
+			BU::Main::Directories::Make "$__BU_MAIN_DOCS" "$__target_path_init" && __folder_path="$__BU_MAIN_DOCS/$__target_path_init"
+			;;
+		6)
+			BU::Main::Directories::Make "$__BU_MAIN_DOCS" "$__target_path_main_functions" && __folder_path="$__BU_MAIN_DOCS/$__target_path_main_functions"
 			;;
 		*)
-			# shellcheck disable=SC2016
-			BU::Main::Errors::HandleErrors "1" "THE $(BU::Main::Echo::ToLowercase "$(BU::DechoE '$__read_doc_name')'s")) ENTERED VALUE IS INVALID" "Please type an integer value ranging from 1 to 5" "$__read_folder_code" "$(basename "${BASH_SOURCE[0]}")" "${FUNCNAME[0]}" "$lineno_case_read_folder_is_valid"
+            if [ -n "$__ARG_TYPE" ]; then
+                BU::Main::Errors::HandleErrors "1" "THE SECOND ARGUMENT'S VALUE ($__ARG_TYPE) IS INVALID" \
+                    "Please type an integer value ranging from 1 to 5" "$__read_folder_code" "$(basename "${BASH_SOURCE[0]}")" "${FUNCNAME[0]}" "$lineno_case_read_folder_is_valid"
+            
+            else
+                # shellcheck disable=SC2016
+                BU::Main::Errors::HandleErrors "1" "THE $(BU::DechoE '$__read_doc_name')'s) ENTERED VALUE IS INVALID" \
+                    "Please type an integer value ranging from 1 to 5" "$__read_folder_code" "$(basename "${BASH_SOURCE[0]}")" "${FUNCNAME[0]}" "$lineno_case_read_folder_is_valid"
+            fi
 			;;
 	esac
 
-	BU::EchoNewstep "How do you want to name your document ?"
-	read -rp "Enter the file's name (no ''.tek'' extension, this script will complete it) : " __read_doc_name
-	BU::EchoRead "$__read_doc_name"
+	if [ -z "$__ARG_FILENAME" ]; then
+        BU::EchoNewstep "How do you want to name your document ?"
+        read -rp "Enter the file's name (no ''.tek'' extension, this script will complete it) : " __read_doc_name
+        BU::EchoRead "$__read_doc_name"
+    fi
 
 	if [ -z "$__read_doc_name" ]; then
 		# shellcheck disable=SC2016
