93c2a4b812187006f2c136e2c0dc75504d67aa14
Dimob <obeiddimitri@gmail.com>
2022-02-25 10:38:20 +0100

    Big save

---
+++ lib/functions/Hardware/CPU.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# ---------------------
+# SCRIPT'S INFORMATIONS
+
+# Name          : CPU.lib
+# Module        : Hardware
+# Description   : Functions used to process information about the CPU.
+# Author(s)     : Dimitri Obeid
+# Version       :
+
+# ----------------------------
+# SHELLCHECK GLOBAL DISABLER :
+
+# Add a coma after each warning code to disable multiple warnings at one go.
+
+# Do not uncomment the "shellcheck disable" line, or else the shellcheck command will be executed during the script's execution, and will not detect any coding mistake during a debugging process.
+
+# DO NOT PUT A COMA AFTER A SHELLCHECK CODE IF THERE'S NO OTHER SHELLCHECK CODE FOLLOWING IT, OR ELSE SHELLCHECK WILL RETURN ERRORS DURING THE DEBUGGING PROCESS !!!
+
+# shellcheck disable=SC2154
+
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# DO NOT EXECUTE THIS SCRIPT DIRECTLY, instead, just source it by calling the "$__BU_MAIN_FUNCTIONS_FILES_PATH" array in the initializer file.
+
+# /////////////////////////////////////////////////////////////////////////////////////////////// #
+
+# Preventing the direct execution of this file, as this script is not meant to be directly executed, but sourced.
+if [ "${0##*/}" == "${BASH_SOURCE[0]##*/}" ]; then
+    echo -e "WARNING !" >&2; echo >&2;
+    echo -e "This shell script (${BASH_SOURCE[0]}) is not meant to be executed directly !" >&2;
+    echo -e "Use this script only by sourcing it in your project script." >&2; echo >&2;
+
+    exit 1;
+fi
+
+# ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; #
+
+#################################### DEFINING LIBRARY FUNCTIONS ###################################
+
+#### PROCCESSING THE CPU INFORMATIONS
+
+## GET CPU INFORMATIONS
+
+# Getting the number of cores.
+function BU::Hardware::CPU::GetCoresNumber() { EchoMsg "$(nproc)" '-n'; }
+
+# Getting the CPU architecture.
+function BU::Hardware::CPU::GetArch() { EchoMsg "$(uname -m)" '-n'; }
+
+# Getting the CPU vendor
+function BU::Hardware::CPU::GetVendor() { EchoMsg "$(lshw -class processor | grep )" '-n'; }
---
+++ res/tests/check_if_variable_is_array.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+function check_if_variable_is_array()
+{
+    if declare -p "$1" 2>/dev/null | grep -q 'declare -a'; then
+        echo "Array"
+    else
+        echo "Not an array"
+    fi
+}
+
+check_if_variable_is_array "$1"
