eafae1ebb4cb5c9f76f10a8a53516e150cdd0141
Dimob <obeiddimitri@gmail.com>
2022-03-10 11:10:54 +0100

    Saving

---
+++ F.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function BU::Hardware::DriveFS::GetFSDevice()
+{
+    #**** Parameters ****
+    local p_path=$1     # String    - Path of the file or the folder to process.
+
+    #**** Variables ****
+    local v_warn; v_warn="$(BU::DechoHighlightFunction "${FUNCNAME[0]}") --> Warning : unable to get the target ($(BU::DechoHighlightPath "$p_path") file system's device";
+
+    BU::IsOSHaiku && local v_cmd="df \"$p_path\" | awk 'FNR == 2 {print \$5}' || { BU::EchoWarning \"\$v_warn\" '-n' 'nodate'; return 1; }";
+    BU::IsOSLinux && local v_cmd="df -Th \"$p_path\" | awk 'FNR == 2 {print \$1}' || { BU::EchoWarning \"\$v_warn\" '-n' 'nodate'; return 1; }";
+
+    #**** Code ****
+    if ! BU::Main::CMDS::GetCommandReturnOutputValue 'df "$path"'; then return 1; fi
+
+    eval "$v_cmd";
+
+    return 0;
+}
--- LibTest.sh
+++ LibTest.sh
@@ -13,6 +13,7 @@ fi
 if ! BashUtils_InitModules \
     "module --log-display --mode-log-partial" \
     "main --stat-debug=true stat-error=fatal --stat-log=true --stat-log-r=tee --stat-time-txt=1 --stat-txt-fmt=true" \
+    "Hardware"
 
     then
 	    echo >&2; echo "In $(basename "$0"), line $(( LINENO-1 )) --> Error : something went wrong while calling the « BashUtils_InitModules() » function" >&2; echo >&2; exit 1
