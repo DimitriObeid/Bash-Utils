df185897138f9430ebbf5d3e35228e03b1c2538f
Dimob <obeiddimitri@gmail.com>
2022-04-12 18:06:02 +0200

    Saving

--- install/.Bash-utils/modules/main/Initializer.sh
+++ install/.Bash-utils/modules/main/Initializer.sh
@@ -344,7 +344,7 @@ done
 ## CALLING NECESSARY FUNCTIONS
 
 # Translating the main module's library messages
-BU::ModuleInit::ParseCSVLang "$__BU_MODULE_INIT__USER_LANG" "$__BU_MODULE_INIT__CSV_TRANSLATION_FILE__DELIM";
+# BU::ModuleInit::ParseCSVLang "$__BU_MODULE_INIT__USER_LANG" "$__BU_MODULE_INIT__CSV_TRANSLATION_FILE__DELIM";
 
 # Since the "CheckProjectLogFile()" function manages the text displaying if the log file doesn't exists, it's okay to call this function now.
 BU::Main::Directories::MkTmpDir;
