3195d8d9c2e25fc15d7bb2e6aec88845f44800e0
Dimitri OBEID <obeiddimitri@gmail.com>
2022-04-28 08:18:16 +0200

    Creating a Powershell script for the Windows installation

---
+++ install/WinInstallGUI-CLI.ps1
@@ -0,0 +1,8 @@
+﻿Add-Type -assembly System.Windows.Forms
+
+$main_form = New-Object System.Windows.Forms.Form
+$main_form.Text = 'Bash Utils : CLI and GUI installer for Windows'
+$main_form.Width = 600
+$main_form.Height = 400
+$main_form.AutoSize = $true
+$main_form.ShowDialog()
