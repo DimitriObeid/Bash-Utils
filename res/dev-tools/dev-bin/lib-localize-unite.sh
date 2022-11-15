bad113d2dd2c30f8997c522a54e22fac3fa01b73
Dimob <obeiddimitri@gmail.com>
2022-11-15 14:40:18 +0100

    Adding a script to create localized files containing the framework's main resources

--- res/dev-tools/dev-bin/lib-localize-unite.sh
+++ res/dev-tools/dev-bin/lib-localize-unite.sh
@@ -44,6 +44,8 @@ __BU_MODULE_INIT_TRANSLATIONS_PATH="$__BU_MODULE_INIT_CONFIGS_PATH/locale";
 ## COLORS
 
 # Defining some colors for the text decoration.
+__CYAN="$(tput setaf 51)";
+
 __GREEN=$(tput setaf 2);
 
 __ORANGE=$(tput setaf 166);
@@ -59,15 +61,21 @@ __YELLOW=$(tput setaf 11);
 ## MESSAGE TRANSLATIONS
 
 if [[ "$LANG" = fr_* ]]; then
-    __BU_COMPILE__SHELLCHECK_MISSING="${__RED}La commande ${__ORANGE}Shellcheck${__RED} n'est pas installée sur votre système !${__RESET}";
+    __BU_COMPILE__SHELLCHECK_MISSING="${__RED}LA COMMANDE ${__CYAN}SHELLCHECK${__RED} N'EST PAS INSTALLÉE SUR VOTRE SYSTÈME !${__RESET}";
+    __BU_COMPILE__SHELLCHECK_VERIFICATION="";
+    __BU_COMPILE__SHELLCHECK_FAIL="${__RED}UNE OU PLUSIEURS ERREURS DE PROGRAMMATION ONT ÉTÉ DÉTECTÉES DANS LE FICHIER ${__CYAN}%s${__RED} !${__RESET}";
+    __BU_COMPILE__SHELLCHECK_SUCCESS="";
 
-    __BU_COMPILE__BAD_LANGUAGE_PASSED="${__RED}${__RESET}";
+    __BU_COMPILE__BAD_LANGUAGE_PASSED="${__RED}Un code de langue ISO 639-1 non-supporté a été passé en premier argument de la fonction ${__CYAN}CompileInSingleFile()${__RED} !${__RESET}";
 
 # Since no other languages are supported, English is set as the default language.
 else
-    __BU_COMPILE__SHELLCHECK_MISSING="${__RED}The ${__ORANGE}Shellcheck${__RED} command is not installed on your system!${__RESET}";
+    __BU_COMPILE__SHELLCHECK_MISSING="${__RED}THE ${__CYAN}SHELLCHECK${__RED} COMMAND IS NOT INSTALLED ON YOUR SYSTEM!${__RESET}";
+    __BU_COMPILE__SHELLCHECK_VERIFICATION="${__ORANGE}Verifying the programming errors from the ${__CYAN}$p_path${__ORANGE} file${__RESET}";
+    __BU_COMPILE__SHELLCHECK_FAIL="${__RED}ONE OR MORE PROGRAMMING ERRORS WERE DETECTED IN THE ${__CYAN}%s${__RED} FILE!${__RESET}";
+    __BU_COMPILE__SHELLCHECK_SUCCESS="${__GREEN}The ${__CYAN}${__GREEN} file doen't contain any programming errors${__RESET}";
 
-    __BU_COMPILE__BAD_LANGUAGE_PASSED="${__RED}${__RESET}";
+    __BU_COMPILE__BAD_LANGUAGE_PASSED="${__RED}An unsupported language ISO 639-1 code was passed as ${__CYAN}CompileInSingleFile()${__RED} function's first argument ${__RESET}";
 fi
 
 # -----------------------------------------------
@@ -87,6 +95,15 @@ function PrintLine()
             printf '-';
     done; printf "\n";
 }
+
+# Printing an error line.
+function PrintErrorLine()   { echo >&2; printf "%s" "$__RED"; PrintLine; printf "%s" "$__RESET"; echo "$1"; printf "%s" "$__RED"; PrintLine; printf "%s" "$__RESET"; echo >&2; }
+
+# Printing a newstep line.
+function PrintNewstepLine() { echo; printf "%s" "$__ORANGE"; PrintLine; printf "%s" "$__RESET"; echo "$1"; printf "%s" "$__ORANGE"; PrintLine; printf "%s" "$__RESET"; echo; }
+
+# Printing a success line.
+function PrintSuccessLine   { echo; printf "%s" "$__GREEN"; PrintLine; printf "%s" "$__RESET"; echo "$1"; printf "%s" "$__GREEN"; PrintLine; printf "%s" "$__RESET"; echo; }
 
 # Converts a byte count to a human readable format in IEC binary notation (base-1024 (eg : GiB)), rounded to two
 # decimal places for anything larger than a byte. Switchable to padded format and base-1000 (eg : MB) if desired.
@@ -130,10 +147,6 @@ function BytesToHuman()
     return 0;
 }
 
-# function BU.Main.Echo.Newline { local iterations="$1"; for ((i=0; i<iterations; i++)); do echo -e "" | tee -a "$__BU_MAIN_FULL_FILE_PATH"; done; }
-function CatBU { cat "$1" | tee -a "$__BU_MAIN_FULL_FILE_PATH" || { echo "UNABLE TO DISPLAY THE $1 FILE'S CONTENT ! Please check its path and if it exists."; exit 1; }; }
-function EchoBU { echo -e "# $1" | tee -a "$__BU_MAIN_FULL_FILE_PATH" || { echo "UNABLE TO WRITE THE $1 FILE'S CONTENT ! Please check its path and if it exists."; exit 1; }; }
-
 # Verifying the list of every ISO 639-1 language codes.
 function CheckISO639_1_LangCode()
 {
@@ -150,7 +163,51 @@ function CheckISO639_1_LangCode()
                         'ta' 'tt' 'te' 'th' 'bo' 'ti' 'to' 'ts' 'tn' 'tr' 'tk' 'tw' 'ug' 'uk' 'ur' 'uz' 've' 'vi' 'vo' 'wa' 'cy' 'wo' 'xh' 'yi' 'yo' 'za' 'zu')
 
     #**** Code ****
-    [[ ${va_langArray[*]} =~ "$p_code" ]] && return 0; return 1;
+    [[ ${va_langArray[*]} =~ ${p_code,,} ]] && return 0; return 1;
+}
+
+# Writing the error message if a Shellcheck verification failed.
+function ShellcheckError()
+{
+    #**** Parameters ****
+    local p_path=${1};  # String    - Default : NULL    - Path of the file whose Shellcheck verification failed.
+
+    #**** Code ****
+    echo >&2;
+
+    # shellcheck disable=SC2059
+    PrintErrorLine "$(printf "$__BU_COMPILE__SHELLCHECK_FAIL" "$p_path")"; return 1;
+}
+
+# Verifying any programming errors with the Shellcheck command.
+function ShellcheckVerif()
+{
+    #**** Parameters ****
+    local p_path=${1};  # String    - Default : NULL    - Path of the file to verify.
+
+    #**** Code ****
+    echo "$__BU_COMPILE__SHELLCHECK_VERIFICATION";
+
+    shellcheck "$p_path" || { ShellcheckError "$p_path"; return 1; }
+
+    echo "$__BU_COMPILE__SHELLCHECK_SUCCESS";
+
+    echo;
+
+    return 0;
+}
+
+# Writing the target file's content into the file to generate.
+function WriteBU()
+{
+    #**** Parameters ****
+    local p_content=${1:-\$'0'} # String    - Default : NULL    -
+    local p_display=${2:-no};   # String    - Default : no      - Display the content of each file when it is read and written into the file to generate.
+
+    #**** Code ****
+    if      [ "${p_display,,}" == 'yes' ]; then echo -e "# $p_content" | tee -a "$__BU_MAIN_FULL_FILE_PATH" || { PrintErrorLine "UNABLE TO WRITE THE FILE'S CONTENT IN THE FILE TO GENERATE ! Please check its path and if it exists."; return 1; };
+    else                                        echo -e "# $p_content" >>       "$__BU_MAIN_FULL_FILE_PATH" || { PrintErrorLine "UNABLE TO WRITE THE FILE'S CONTENT IN THE FILE TO GENERATE ! Please check its path and if it exists."; return 1; };
+    fi
 }
 
 # ----------------------------------------------
@@ -162,7 +219,7 @@ function CheckISO639_1_LangCode()
 ######################################################### CODE ########################################################
 
 # Checking first if Shellcheck is installed in order to check for code errors.
-if ! command -v shellcheck; then echo >&2; echo "$__BU_COMPILE__SHELLCHECK_MISSING" >&2; echo >&2; exit 1; fi
+if ! command -v shellcheck; then PrintErrorLine "$__BU_COMPILE__SHELLCHECK_MISSING" >&2; exit 1; fi
 
 # Files compilation function.
 function CompileInSingleFile()
@@ -173,10 +230,78 @@ function CompileInSingleFile()
 
     #**** Code ****
     # Checking if the "$p_locale" variable is a valid ISO 639-1 language code.
-    if ! CheckISO639_1_LangCode "$p_locale"; then echo >&2; echo "" >&2; echo >&2; exit 1; fi
+    if ! CheckISO639_1_LangCode "$p_locale"; then PrintErrorLine "$__BU_COMPILE__BAD_LANGUAGE_PASSED"; return 1; fi
+
+    # Writing the initializer script's translations files content first into the compiled file.
+    if  [ ! -f "$__BU_MODULE_INIT_TRANSLATIONS_PATH/${p_locale}.locale" ]; then PrintErrorLine "" >&2; return 1;
+    else
+        ShellcheckVerif "$__BU_MODULE_INIT_TRANSLATIONS_PATH/${p_locale}.locale";
+
+        WriteBU "$__BU_MODULE_INIT_TRANSLATIONS_PATH/${p_locale}.locale" "$p_display";
+    fi
+
+    PrintNewstepLine "";
+
+    # Writing the main module's library files content into the compiled file.
+    for i in "$__BU_ROOT_PATH/lib/functions/main/"*.lib; do
+        ShellcheckVerif "${i}";
+
+        WriteBU "${i}" "$p_display";
+    done
+
+    PrintSuccessLine "";
+
+    PrintNewstepLine "";
+
+    # Writing the main module's configuration files content into the compiled file.
+    for i in "$HOME/.Bash-utils/config/modules/main/"*.conf; do
+        ShellcheckVerif "${i}";
+
+        WriteBU "${i}" "$p_display";
+    done
+
+    PrintSuccessLine "";
+
+    PrintNewstepLine "";
+
+    # Writing the main module's initializer script's content into the compiled file.
+    for i in "$HOME/.Bash-utils/modules/main/"*; do
+        ShellcheckVerif "${i}";
+
+        WriteBU "${i}" "$p_display";
+    done
+
+    PrintSuccessLine "";
+
+    PrintNewstepLine "";
+
+    # Writing the initializer script's configuration files content into the compiled file.
+    for i in "$__BU_MODULE_INIT_CONFIGS_PATH/"*.conf; do
+        ShellcheckVerif "${i}";
+
+        WriteBU "${i}" "$p_display";
+    done
+
+    PrintSuccessLine "";
+
+    PrintNewstepLine "";
+
+    # Writing the initializer script's content.
+    if [ ! -f "$__BU_MAIN_FULL_FILE_PATH" ]; then PrintErrorLine ""; return 1;
+    else
+        ShellcheckVerif "$__BU_MAIN_FULL_FILE_PATH";
+
+        WriteBU "$__BU_MAIN_FULL_FILE_PATH" "$p_display";
+    fi
+
+    PrintSuccessLine "";
 
     return 0;
 }
 
-CompileInSingleFile "en" "yes";
-CompileInSingleFile "fr" "yes";
+
+CompileInSingleFile "en" "yes" || { PrintErrorLine "IMPOSSIBLE TO CREATE AN ENGLISH VERSION CONTAINING THE MAIN RESOURCES OF THE FRAMEWORK ENCAPSULATED IN A SINGLE FILE!!!"; exit 1; };
+PrintSuccessLine "SUCCESSFULLY CREATED AN ENGLISH VERSION CONTAINING THE MAIN RESOURCES OF THE FRAMEWORK ENCAPSULATED IN A SINGLE FILE";
+
+CompileInSingleFile "fr" "yes" || { PrintErrorLine "IMPOSSIBLE DE CRÉER UNE VERSION FRANÇAISE CONTENANT LES PRINCIPALES RESSOURCES DU FRAMEWORK ENCAPSULÉES EN UN SIMPLE FICHIER !!!"; exit 1; };
+PrintSuccessLine "CRÉATION D'UNE VERSION FRANÇAISE CONTENANT LES PRINCIPALES RESSOURCES DU FRAMEWORK ENCAPSULÉES DANS UN SEUL FICHIER RÉUSSIE AVEC SUCCÈS";
