6c6a38459de38d62bf037770fca72eae4461cb71
Dimob <obeiddimitri@gmail.com>
2022-11-19 18:47:56 +0100

    Added the "BU.ModuleInit.IsFrameworkBeingInstalled()" function, improved the "BU.ModuleInit.IsInScript()" function and added a global variable containing the path to the framework's temporary directory

---
+++ res/tests/IsInScript.sh
@@ -0,0 +1,7 @@
+echo "Fichier : $0"; echo;
+
+function x() { echo "Fichier fnct : $0"; }; x; echo;
+
+function y() { local str="Hello"; echo "${str: -2}"; }; y; echo;
+
+function z() { local v_name="$0"; local v_str="${v_name: -3}"; echo "VARIABLE : $v_str"; echo; if [[ "${v_str^^}" == .SH ]]; then echo TRUE; else echo FALSE; fi; }; z;
