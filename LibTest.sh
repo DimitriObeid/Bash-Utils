a5afa95da25ea3552252ffe9dc9ac2d5f192d7c6
Dimob <obeiddimitri@gmail.com>
2022-12-31 18:52:19 +0100

    Corrected a small display bug in the "Filesystem.conf" file

--- LibTest.sh
+++ LibTest.sh
@@ -16,8 +16,7 @@ fi
 # Calling the "BashUtils_InitModules()" function.
 if ! BashUtils_InitModules \
     "module --log-display --mode-log-partial --stat-debug=true --stat-debug-bashx=file" \
-    "main --stat-debug=true stat-error=fatal --stat-log=true --stat-log-r=tee --stat-time-txt=1 --stat-txt-fmt=true" \
-    "Hardware"
+    "main --stat-debug=true stat-error=fatal --stat-log=true --stat-log-r=tee --stat-time-txt=1 --stat-txt-fmt=true";
 
     then
 	    echo >&2; echo "In $(basename "$0"), line $(( LINENO-1 )) --> Error : something went wrong while calling the « BashUtils_InitModules() » function" >&2; echo >&2; exit 1;
