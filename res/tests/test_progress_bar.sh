db556242de75eba9572d3b380bc0992eb6849280
Dimitri Obeid <obeiddimitri@gmail.com>
2023-01-17 10:16:27 +0100

    Saving

--- res/tests/test_progress_bar.sh
+++ res/tests/test_progress_bar.sh
@@ -6,16 +6,16 @@ function BU.Main.PosixTerm.DisplayProgressBarOld()
         v_cols="$(tput cols)"
 
         # Process data.
-        let _progress=$(( (${1}*100/${2}*100)/100 ))
-        let _done=$(( (${_progress}*4)/10 ))
-        let _left=40-$_done
+        let _progress=$(( (${1}*100/${2}*100)/100 ));
+        let _done=$(( (${_progress}*4)/10 ));
+        let _left=40-${_done};
 
         # Build progressive string lengths.
-        _fill=$(printf "%${_done}s")
-        _empty=$(printf "%${_left}s")
+        _fill=$(printf "%${_done}s");
+        _empty=$(printf "%${_left}s");
 
         # Printing the bar
-        printf "\rProgress : [ ${_fill// /#}${_empty// /-}] ${_progress}%%"
+        printf "\rProgress : [ ${_fill// /#}${_empty// /-}] ${_progress}%%";
 
 	return 0;
 }
@@ -24,30 +24,30 @@ function BU.Main.PosixTerm.DisplayProgressBarOld()
 # Display a progress bar while a task is ongoing.
 function BU.Main.PosixTerm.DisplayProgressBar()
 {
-	v_cols="$(tput cols)"
+	v_cols="$(tput cols)";
 
 	# Process data.
 	let _progress=$(( (${1}*100/${2}*100)/100 )) 	|| return 1;
-	let _done=$(( (${_progress}*4)/10 ))		|| return 1;
-	let _left=40-$_done				|| return 1;
+	let _done=$(( (${_progress}*4)/10 ))	|| return 1;
+	let _left=40-${_done}		    || return 1;
 
 	# Build progressive string lengths.
-	_fill=$(printf "%${_done}s")
-	_empty=$(printf "%${_left}s")
+	_fill=$(printf "%${_done}s");
+	_empty=$(printf "%${_left}s");
 
 	# Printing the bar
-	printf "\rProgress : [ ${_fill// /#}${_empty// /-}] ${_progress}%%"
+	printf "\rProgress : [ ${_fill// /#}${_empty// /-}] ${_progress}%%";
 
 	return 0;
 }
 
-__start='1'
+__start='1';
 
-__end="$(tput cols)"
+__end="$(tput cols)";
 
 for number in $(seq "${__start}" "${__end}"); do
-	sleep "0.1"
-	BU.Main.PosixTerm.DisplayProgressBar "${number}" "${__end}"
+	sleep "0.1";
+	BU.Main.PosixTerm.DisplayProgressBar "${number}" "${__end}";
 done
 
-printf "\nDONE\n"
+printf "\nDONE\n";
