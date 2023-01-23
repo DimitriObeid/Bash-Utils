dbcef35332cc6ae1570d0872e8a0df9bdce1272c
Dimitri Obeid <obeiddimitri@gmail.com>
2023-01-23 09:44:25 +0100

    Renamed the "latex-compilation-recursive.sh" file to "latex-compiler.sh"

--- res/dev-tools/dev-bin/latex-compiler.sh
+++ res/dev-tools/dev-bin/latex-compiler.sh
@@ -3,14 +3,14 @@
 # ----------------------------------------
 # DEV-TOOLS EXECUTABLE FILE INFORMATIONS :
 
-# Name          : latex-compilation-recursive.sh
+# Name          : latex-compiler.sh
 # Author(s)     : Dimitri OBEID
 # Version       : 1.0
 
 # ------------------
 # FILE DESCRIPTION :
 
-# This script recursively compiles EVERY ".tex" files from a specific folder.
+# This script recursively compiles EVERY ".tex" files from the project's root folder.
 
 # ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; #
 
