afd1053f045eb86166217059003d1a0938c01ceb
Dimitri Obeid <obeiddimitri@gmail.com>
2023-01-31 11:28:40 +0100

    The "BU.Main.Inputs.Sanitize()" string sanitizer function is now functionning

--- res/tests/Sanitize.sh
+++ res/tests/Sanitize.sh
@@ -3,16 +3,16 @@
 function BU.Main.Inputs.Sanitize()
 {
     #**** Parameters ****
-    local p_string=${1:-$'\0'}; # ARG TYPE : String     - REQUIRED | DEFAULT VAL : NULL     - DESC : String to convert in a word array.
+    local p_string=${1:-Hello}; # ARG TYPE : String     - REQUIRED | DEFAULT VAL : NULL     - DESC : String to convert in a word array.
 
     #**** Variables ****
     local v_dangerousChars=';|&|\||`|>|<|$|(|)|{|}';
-    local v_dangerousCmds='rm|mv|cp|mkdir|rmdir|ssh|ftp|wget|curl|dd';
+    local v_dangerousCmds='rm|mv|cp|mkdir|rmdir|ssh|ftp|wget|curl|dd|shred|eval|sh|bash|dash|ksh|zsh|mkfs|unzip|uz|gunzip|7z|unrar';
     local v_specialChars='[^a-zA-Z0-9_\./-]';
 
     #**** Code ****
     # Checking if the value does not contain dangerous commands.
-    if echo "${p_string}" | grep -E -q "${v_dangerousChars}"; then
+    if ! echo "${p_string}" | grep -E -q "${v_dangerousChars}"; then
         echo "THE '' ${p_string} '' INPUT CONTAINS DANGEROUS COMMANDS AND THUS CANNOT BE EVALUATED !!!!!" \
 
         return 1;
@@ -35,4 +35,4 @@ function BU.Main.Inputs.Sanitize()
     return 0;
 }
 
-BU.Main.Inputs.Sanitize "${1:-Hello}";
+BU.Main.Inputs.Sanitize "${1:-Hello|}";
