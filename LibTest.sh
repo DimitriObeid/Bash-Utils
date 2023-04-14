7e86aaee5c938161389001cd6f627ac1c2e19240
Dimitri Obeid <obeiddimitri@gmail.com>
2023-04-14 17:15:45 +0200

    Sauvegarde

--- LibTest.sh
+++ LibTest.sh
@@ -22,7 +22,7 @@ fi
 # Calling the "BashUtils_InitModules()" function.
 if ! BashUtils_InitModules \
     "module --log-display --mode-log-partial --stat-debug=false --stat-debug-bashx=file --include-aliases=main,hardware" \
-    "main --stat-debug=true stat-error=fatal --stat-log=true --stat-log-r=tee --stat-time-txt=1 --stat-txt-fmt=true" \
+    "main --stat-debug=true stat-error=fatal --stat-log=true --stat-log-r=tee --stat-time-header=0 --stat-time-newline=0 --stat-time-txt=0 --stat-txt-fmt=true" \
 
     then
 	    echo >&2; echo "In $(basename "${0}"), line $(( LINENO - 1 )) --> Error : something went wrong while calling the « BashUtils_InitModules() » function" >&2; echo >&2; exit 1;
